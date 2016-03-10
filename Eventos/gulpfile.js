fe4827b400d15e6092bd7f14addc489ccdb2fded
Grabowski <sylwester.grabowski@capgemini.com>
2016-03-10 21:18:47 +0100

    fixed watch

--- Eventos/gulpfile.js
+++ Eventos/gulpfile.js
@@ -2,39 +2,36 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 
 
-gulp.task('transpile:js', function() {
-    
- var tsProject = ts.createProject('app.js/tsconfig.json');
+gulp.task('transpile:js', function () {
 
-    var tsResult = tsProject.src()
-        .pipe(ts(tsProject));  
-
+    var tsProject = ts.createProject('app.js/tsconfig.json');
+    var tsResult = tsProject.src().pipe(ts(tsProject));
     return tsResult.js.pipe(gulp.dest("wwwroot/app"));
-        
+
 });
-gulp.task('copy:libs', function() {
-  return gulp.src([
-      'node_modules/angular2/bundles/angular2-polyfills.js',
-      'node_modules/systemjs/dist/system.src.js',
-      'node_modules/rxjs/bundles/Rx.js',
-      'node_modules/angular2/bundles/angular2.dev.js',
-      'node_modules/angular2/bundles/router.dev.js',
-      'node_modules/angular2/bundles/http.dev.js'
+gulp.task('copy:libs', function () {
+    return gulp.src([
+        'node_modules/angular2/bundles/angular2-polyfills.js',
+        'node_modules/systemjs/dist/system.src.js',
+        'node_modules/rxjs/bundles/Rx.js',
+        'node_modules/angular2/bundles/angular2.dev.js',
+        'node_modules/angular2/bundles/router.dev.js',
+        'node_modules/angular2/bundles/http.dev.js'
     ])
-    .pipe(gulp.dest('wwwroot/lib'))
+        .pipe(gulp.dest('wwwroot/lib'))
 });
 
-gulp.task('copy:others', function() {
-    return gulp.src(["app.js/**/*.html","app.js/**/*.css"])
+gulp.task('copy:others', function () {
+    return gulp.src(["app.js/**/*.html", "app.js/**/*.css"])
         .pipe(gulp.dest("wwwroot"));
 });
 
-gulp.task('default', ['copy:libs','transpile:js','copy:others'], function() {
+gulp.task('default', ['copy:libs', 'transpile:js', 'copy:others'], function () {
 
 });
 
 
-gulp.task('watch', ['default'], function() {
-    gulp.watch(["app.js/**/*.ts"],'transpile:js');
-    gulp.watch([!"app.js/**/*.ts"],'copy:others');
+gulp.task('watch', ['default'], function () {
+    gulp.watch(["app.js/**/*.ts"],[ 'transpile:js']);
+    gulp.watch(["app.js/**/*.html"], ['copy:others']);
 });
